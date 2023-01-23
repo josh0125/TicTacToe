b272cc3a262b93ed6351506892d0e113cfa10c63
Jon Stauffer <staufjon@byu.edu>
2023-01-23 13:46:38 -0700

    1

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,6 +7,10 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic-Tac-Toe 2.0!");
+            support sup = new support();
+            char[] array = {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' };
+            sup.printBoard(array);
+            Console.ReadLine();
 
 
         }
---
+++ TicTacToe/support.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class support
+    {
+        public void printBoard(char[] board)
+        {
+            int[] test = { 0, 1, 3, 4, 6, 7 };
+            for(int i = 0; i < board.Length; ++i)
+            {
+                Console.Write(board[i]);
+
+                if (test.Contains(i))
+                {
+                    Console.Write('|');
+                }
+                
+                if ((i == 2) || (i == 5))
+                {
+                    Console.WriteLine("-------");
+                }
+                
+            }
+        }
+        //int winner(char[] board)
+       
+    }
+}
