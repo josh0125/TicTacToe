2c48a9a617616b7fa2ef34670a036c037553d8f4
Jon Stauffer <staufjon@byu.edu>
2023-01-25 12:34:11 -0700

    commit

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,8 +6,83 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+
+            char[] gameBoard = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            bool winner = false; //checks if someone has won the game
+
+            int count = 1; //keeps track of number of times the loop executes
+
+            string currentPlayer;
+
+            string gameWinner;
+
+            support sup = new support();
+
             Console.WriteLine("Welcome to Tic-Tac-Toe 2.0!");
 
+            while (!winner && count <= gameBoard.Length)
+            {
+                int playerChoice;
+
+                char token = 'X';
+
+                Console.WriteLine();
+
+                sup.printBoard(gameBoard);
+
+                if (count % 2 == 0) //alternates players on even and odd turns
+                {
+                    currentPlayer = "Player 2";
+                    token = 'O';
+                }
+
+                else
+                {
+                    currentPlayer = "Player 1";
+                }
+
+                Console.Write(currentPlayer + " choose a position: ");
+                playerChoice = Convert.ToInt32(Console.ReadLine());
+
+                while (gameBoard[playerChoice - 1] == 'X' || gameBoard[playerChoice - 1] == 'O') //loop catches error if position is already in use
+                {
+                    Console.Write("This position has already been taken, " + currentPlayer + " please choose another position: ");
+                    playerChoice = Convert.ToInt32(Console.ReadLine());
+                }
+
+                gameBoard[playerChoice - 1] = token;
+                count += 1;
+
+                if (sup.winner(gameBoard)[0] == 1) //check if a winner exists
+                {
+                    winner = true;
+
+                    if (sup.winner(gameBoard)[1] == 1)
+                    {
+                        gameWinner = "Player 1";
+                    }
+
+                    else
+                    {
+                        gameWinner = "Player 2";
+                    }
+
+                    Console.WriteLine();
+
+                    sup.printBoard(gameBoard);
+
+                    Console.WriteLine(gameWinner + " wins!");
+                }
+
+            }
+
+            if (!winner) //execute when count exceeds board length without a winner
+            {
+                Console.WriteLine();
+                sup.printBoard(gameBoard);
+                Console.WriteLine("It's a draw!");
+            }
 
         }
     }
---
+++ TicTacToe/support.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class support
+    {
+        public void printBoard(char[] board)
+        {
+            int[] test = { 0, 1, 3, 4, 6, 7 };
+            for (int i = 0; i < board.Length; ++i)
+            {
+                Console.Write(board[i]);
+                if (test.Contains(i))
+                {
+                    Console.Write('|');
+                }
+                if ((i == 2) || (i == 5))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("------");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        public int[] winner(char[] board)
+        {
+            int[] decision = { 0, 0 };
+            char finalWinner = ' ';
+            
+            if ((board[0] == board[1]) && (board[1] == board[2]))
+            {
+                finalWinner = board[0];
+            }
+            else if ((board[3] == board[4]) && (board[4] == board[5]))
+            {
+                finalWinner = board[3];
+            }
+            else if ((board[6] == board[7]) && (board[7] == board[8]))
+            {
+                finalWinner = board[6];
+            }
+            else if ((board[0] == board[3]) && (board[3] == board[6]))
+            {
+                finalWinner = board[0];
+            }
+            else if ((board[1] == board[4]) && (board[4] == board[7]))
+            {
+                finalWinner = board[1];
+            }
+            else if ((board[2] == board[5]) && (board[5] == board[8]))
+            {
+                finalWinner = board[2];
+            }
+            else if ((board[0] == board[4]) && (board[4] == board[8]))
+            {
+                finalWinner = board[0];
+            }
+            else if ((board[2] == board[4]) && (board[4] == board[6]))
+            {
+                finalWinner = board[2];
+            }
+
+
+
+            if (finalWinner == 'X')
+            {
+                decision[0] = 1;
+                decision[1] = 1;
+            }
+            else if (finalWinner == 'O')
+            {
+                decision[0] = 1;
+                decision[1] = 2;
+            }
+            return (decision);
+        } 
+    }
+}
